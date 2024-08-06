482369233dec5aa33260e0d3e0d97a9d3c622488
Behnam <behnam.dev@outlook.com>
2024-08-07 00:16:51 +0330

    Update PlaylistProcessor.cs

--- Nute.Domain/PlaylistProcessor.cs
+++ Nute.Domain/PlaylistProcessor.cs
@@ -2,6 +2,27 @@ namespace Nute.Domain;
 
 public class PlaylistProcessor
 {
+    public static ComparePlaylistsResult ComparePlaylists(IEnumerable<string> playlist1, IEnumerable<string> playlist2)
+    {
+        var inCommonSongs = new List<string>();
+
+        foreach (var song in playlist1)
+        {
+            if (playlist2.Contains(song))
+            {
+                inCommonSongs.Add(song);
+            }
+        }
+
+        var playlist1Songs = playlist1.ToList();
+        playlist1Songs.RemoveAll(i => inCommonSongs.Contains(i));
+
+        var playlist2Songs = playlist2.ToList();
+        playlist2Songs.RemoveAll(i => inCommonSongs.Contains(i));
+
+        return new ComparePlaylistsResult(playlist1Songs, playlist2Songs, inCommonSongs);
+    }
+
     public static RemoveDuplicateSongsInPlaylistResult RemoveDuplicateSongsInPlaylist(IEnumerable<string> playlist)
     {
         var songs = new HashSet<string>();
@@ -31,26 +52,5 @@ public class PlaylistProcessor
         }
 
         return new FindNonExistentSongsInPlaylistResult(nonExistentSongs);
-    }
-
-    public static ComparePlaylistsResult ComparePlaylists(IEnumerable<string> playlist1, IEnumerable<string> playlist2)
-    {
-        var inCommonSongs = new List<string>();
-
-        foreach (var song in playlist1)
-        {
-            if (playlist2.Contains(song))
-            {
-                inCommonSongs.Add(song);
-            }
-        }
-
-        var playlist1Songs = playlist1.ToList();
-        playlist1Songs.RemoveAll(i => inCommonSongs.Contains(i));
-
-        var playlist2Songs = playlist2.ToList();
-        playlist2Songs.RemoveAll(i => inCommonSongs.Contains(i));
-
-        return new ComparePlaylistsResult(playlist1Songs, playlist2Songs, inCommonSongs);
     }
 }
