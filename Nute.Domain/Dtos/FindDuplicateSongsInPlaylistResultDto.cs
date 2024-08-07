d1ae172c2cea917edd2a8b15fc43ca28216ff90a
Behnam <behnam.dev@outlook.com>
2024-08-08 00:34:38 +0330

    Update FindDuplicateSongsInPlaylistResultDto.cs

--- Nute.Domain/Dtos/FindDuplicateSongsInPlaylistResultDto.cs
+++ Nute.Domain/Dtos/FindDuplicateSongsInPlaylistResultDto.cs
@@ -1,3 +1,3 @@
 namespace Nute.Domain.Dtos;
 
-public record FindDuplicateSongsInPlaylistResultDto(IEnumerable<string> Songs, IEnumerable<string> DuplicateSongs);
+public record FindDuplicateSongsInPlaylistResultDto(IEnumerable<string> UniqueSongs, IEnumerable<string> DuplicateSongs);
