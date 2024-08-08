eddd454199953f9dae3d9c05dd630e0d1a22678f
Behnam <behnam.dev@outlook.com>
2024-08-09 02:00:50 +0330

    Update ComparePlaylistsResultDto.cs

--- Nute.Domain/Dtos/ComparePlaylistsResultDto.cs
+++ Nute.Domain/Dtos/ComparePlaylistsResultDto.cs
@@ -1,3 +1,6 @@
 namespace Nute.Domain.Dtos;
 
-public record ComparePlaylistsResultDto(IEnumerable<string> Playlist1UniqueSongs, IEnumerable<string> Playlist2UniqueSongs, IEnumerable<string> InCommonSongs);
+public record ComparePlaylistsResultDto(
+    IEnumerable<string> Playlist1UniqueSongs,
+    IEnumerable<string> Playlist2UniqueSongs,
+    IEnumerable<string> InCommonSongs);
