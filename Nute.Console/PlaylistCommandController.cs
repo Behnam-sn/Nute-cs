06677fa5f626e268d41a73b2f7a46c924eddce56
Behnam Salehi <behnam.dev@outlook.com>
2024-08-08 23:59:50 +0330

    Create PlaylistCommandController.cs

---
+++ Nute.Console/PlaylistCommandController.cs
@@ -0,0 +1,16 @@
+﻿namespace Nute.Console;
+
+public class PlaylistCommandController
+{
+    internal static void PrintCommands()
+    {
+        System.Console.WriteLine("playlist, m");
+        System.Console.WriteLine("help, h");
+        System.Console.WriteLine("exit, x");
+    }
+
+    internal static void Playlist()
+    {
+        throw new NotImplementedException();
+    }
+}
