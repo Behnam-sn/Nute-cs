d1ea99a0d564c50dfa335999e5b7ee1fb9b8c73e
Behnam Salehi <behnam.dev@outlook.com>
2024-08-09 01:12:53 +0330

    Update VmHelpers.cs

--- Nute.Console/VmHelpers.cs
+++ Nute.Console/VmHelpers.cs
@@ -6,20 +6,20 @@ internal static class VmHelpers
 {
     internal static void ShowInConsole(this ComparePlaylistsResultVm vm)
     {
-        System.Console.WriteLine($"Playlist 1 Title: {vm.Playlist1Title}");
-        System.Console.WriteLine("Playlist 1 Unique Songs:");
+        System.Console.WriteLine($"Playlist 1");
+        System.Console.WriteLine($"Title: {vm.Playlist1Title}");
+        System.Console.WriteLine("Unique Songs:");
         foreach (var song in vm.Playlist1Songs)
         {
             System.Console.WriteLine(song);
         }
-        System.Console.WriteLine("");
-        System.Console.WriteLine($"Playlist 2 Title: {vm.Playlist2Title}");
-        System.Console.WriteLine("Playlist 2 Unique Songs:");
+        System.Console.WriteLine($"Playlist 2");
+        System.Console.WriteLine($"Title: {vm.Playlist2Title}");
+        System.Console.WriteLine("Unique Songs:");
         foreach (var song in vm.Playlist2Songs)
         {
             System.Console.WriteLine(song);
         }
-        System.Console.WriteLine("");
         System.Console.WriteLine("In Common Songs: ");
         foreach (var song in vm.InCommonSongs)
         {
