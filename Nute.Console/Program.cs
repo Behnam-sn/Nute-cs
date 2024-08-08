a0fa1793b0e1b45c0acf021c6c985346d6ab82ec
Behnam Salehi <behnam.dev@outlook.com>
2024-08-09 01:06:14 +0330

    Update Program.cs

--- Nute.Console/Program.cs
+++ Nute.Console/Program.cs
@@ -1,28 +1,9 @@
 ﻿namespace Nute.Console;
 
-public static class Program
+internal static class Program
 {
     private static void Main(string[] args)
     {
-        while (true)
-        {
-            System.Console.Write("Command: ");
-            var command = System.Console.ReadLine()?.ToLower();
-
-            switch (command)
-            {
-                case "help" or "h":
-                    PlaylistCommandController.PrintCommands();
-                    break;
-
-                case "playlist" or "p":
-                    PlaylistCommandController.Playlist();
-                    break;
-
-                case "exit" or "x":
-                    Environment.Exit(0);
-                    break;
-            }
-        }
+        _ = new NuteCommandController();
     }
 }
