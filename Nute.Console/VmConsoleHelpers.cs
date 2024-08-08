1499873d6d66c815c822f4ea63669e2db3ea8351
Behnam Salehi <behnam.dev@outlook.com>
2024-08-09 01:14:25 +0330

    Create VmConsoleHelpers.cs

---
+++ Nute.Console/VmConsoleHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Nute.Application.Vms;
+
+namespace Nute.Console;
+
+internal static class VmConsoleHelpers
+{
+    internal static void ShowInConsole(this ComparePlaylistsResultVm vm)
+    {
+        System.Console.WriteLine($"Playlist 1");
+        System.Console.WriteLine($"Title: {vm.Playlist1Title}");
+        System.Console.WriteLine("Unique Songs:");
+        foreach (var song in vm.Playlist1Songs)
+        {
+            System.Console.WriteLine(song);
+        }
+        System.Console.WriteLine($"Playlist 2");
+        System.Console.WriteLine($"Title: {vm.Playlist2Title}");
+        System.Console.WriteLine("Unique Songs:");
+        foreach (var song in vm.Playlist2Songs)
+        {
+            System.Console.WriteLine(song);
+        }
+        System.Console.WriteLine("In Common Songs: ");
+        foreach (var song in vm.InCommonSongs)
+        {
+            System.Console.WriteLine(song);
+        }
+    }
+}
