9d54d40da98e5c0f621e11edf1776e017668e3c8
Behnam <behnam.dev@outlook.com>
2024-08-19 00:27:57 +0330

    Update SongsManagementService.cs

--- Nute.Application/Songs/SongsManagementService.cs
+++ Nute.Application/Songs/SongsManagementService.cs
@@ -4,9 +4,18 @@ namespace Nute.Application.Songs;
 
 public static class SongsManagementService
 {
-    public static CompareSongsResultVm CompareSongs(string sourcePath, string destinationPath)
+    public static CompareSongsResultVm idk(string sourcePath, string destinationPath)
     {
         return new CompareSongsResultVm();
+    }
+
+    public static void SpecialNeedMethod(string sourcePath, string destinationPath)
+    {
+
+    }
+
+    public static void CompareSongs(string song1Path, string song2Path)
+    {
     }
 
     public static void SyncSongs()
