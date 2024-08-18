1f8b04dbd0c20daee130fd7871bc56b8af304408
Behnam <behnam.dev@outlook.com>
2024-08-19 00:05:54 +0330

    Create VmHelpers.cs

---
+++ Nute.Console/Songs/VmHelpers.cs
@@ -0,0 +1,10 @@
+﻿using Nute.Application.Songs.Vms;
+
+namespace Nute.Console.Songs;
+
+internal static class VmHelpers
+{
+    internal static void PrintInConsole(this CompareSongsResultVm vm)
+    {
+    }
+}
